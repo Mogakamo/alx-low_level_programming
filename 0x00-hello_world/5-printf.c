20fa43b9efbf1a9c1b47039f8ce183339517f81f
Mogakamo <mogaka.amo254@gmail.com>
2021-06-17 12:40:35 +0300

    Hello, printf - 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-/*
- * main - Entry point
- * Description: "\"with proper grammar, but the outcome is a piece of art,"
- * Return: Always 0 (Success)
+/**
+ * main - Prints string
+ * Description: Prints "\"Programming is like building a multilingual puzzle"
+ * Return: 0
  */
 int main(void)
 {
-	printf("%s", "with proper grammar, but the outcome is a piece of art,");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
