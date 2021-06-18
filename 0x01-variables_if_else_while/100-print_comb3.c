9ee3fa1d38bc336c3b9198bbfbbcead5567633ff
Mogakamo <mogaka.amo254@gmail.com>
2021-06-18 18:00:27 +0300

    Commit - 100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,36 +1,35 @@
 #include <stdio.h>
-
 /**
- * main - Entry point
- *
+ * main - prints out all the numbers between 00 and 99
+ * with no two digits being the same
  * Return: 0
  */
 int main(void)
 {
 	int i, j;
 
-	for (i = 48; j < 58; j++)
+	for (i = 48; i < 58; i++)
 	{
 		for (j = i; j < 58; j++)
-		{	
-		if (i == j)
 		{
-			continue;
-		}
+			if (i == j)
+			{
+				continue;
+			}
 
-		putchar(i);
-		putchar(j);
+			putchar(i);
+			putchar(j);
 
-		if (i == 56 && j == 57)
-		{
-			break;
+			if (i == 56 && j == 57)
+			{
+				break;
+			}
+			else
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
-		else
-		{
-			putchar(',');
-			putchar(' ');
-		}
-	    }
 	}
 	putchar('\n');
 	return (0);
