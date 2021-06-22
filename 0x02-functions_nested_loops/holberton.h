5fcd198e8c62677a625564e4e375abc8db4c7597
Mogakamo <mogaka.amo254@gmail.com>
2021-06-22 11:13:33 +0300

    Added the prototypes for number 5 to 11

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -16,4 +16,25 @@ int _islower(int c);
 /* 4-isalpha.c  */
 int _isalpha(int c);
 
+/* 5-sign.c */
+int print_sign(int n);
+
+/* 6-abs.c */
+int _abs(int);
+
+/* 7-print_last_digit.c */
+int print_last_digit(int);
+
+/* 8-24_hours.c */
+void jack_bauer(void);
+
+/* 9 - timestable.c */
+void times_table(void);
+
+/* 10 - add.c */
+int add(int, int);
+
+/* 11-print_to_98.c */
+void print_to_98(int n);
+
 #endif
