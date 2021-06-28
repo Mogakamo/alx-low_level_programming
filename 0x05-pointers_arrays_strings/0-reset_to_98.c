8993cd97f43cff0a5fb2267ce1ff66c793bce2bf
Mogakamo <mogaka.amo254@gmail.com>
2021-06-28 06:11:13 +0300

    working with the reseting program

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,15 +2,11 @@
 
 /**
  * reset_to_98 - function that takes a pointer and updates its value to 98
- * Return: 0
+ * Description: Program that resets to 98
+ * @n: the interger to reset to
+ * 
  */
 void reset_to_98(int *n)
 {
-	int p;
-	int *n;
-
-	p = 98;
-	n = &p;
-
-	return (0);
+	*n = 98;
 }
