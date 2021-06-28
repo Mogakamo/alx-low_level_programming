70bd74303f8581fc993cad9b3e6ea71db5511af3
Mogakamo <mogaka.amo254@gmail.com>
2021-06-28 05:59:55 +0300

    added the first program and also got to modify the the header file for the programs

---
+++ 0x05-pointer_array_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - function that takes a pointer and updates its value to 98
+ * Return: 0
+ */
+void reset_to_98(int *n)
+{
+	int n;
+	int *p;
+
+	n = 98;
+	p = &n;
+
+	return (0);
+}
---
+++ 0x05-pointer_array_strings/holberton.h
@@ -0,0 +1,10 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/* _putchar */
+int _putchar(int n);
+
+/* 0.98 BAttery st */
+void reset_to_98(int *n);
+
+#endif
