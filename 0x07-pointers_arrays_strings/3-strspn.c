0ce791a12818450091edec168b59a416eac3712d
Mogakamo <mogaka.amo254@gmail.com>
2021-07-05 16:44:30 +0300

    commit

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,30 +1,31 @@
 #include "holberton.h"
 #include <stdio.h>
+
 /**
- * _strspn - Gets the length of a prefix substring
- *
- * @s: string to search bytes of
- * @accept: bytes to check
- *
- * Return: count, offset from beginning of s
+ * _strspn - gets the length of a prefix substring.
+ * @s: character to print
+ * @accept: character
+ * Return: i.
  */
 unsigned int _strspn(char *s, char *accept)
 {
-unsigned int count = 0;
-int len = 0;
-int len2 = 0;
+	int count;
 
-for (len2 = 0, count = 0; *(accept + len) != '\0'; len++)
-{
-while (*(s + len2) != '\0')
-{
-if (*(accept + len) == *(s + len2))
-return (count + 1);
-count++;
-len2++;
-}
-}
-if (*(accept + len) != *(s + len))
-return (0);
-return (count);
+	while (*s != '\0')
+	{
+		while (*accept != '\0')
+		{
+			if (*s == *accept)
+				break;
+			count++;
+			accept++;
+
+			s++;
+
+		}
+		if (*accept == '\0')
+			break;
+
+	}
+	return (count + 1);
 }
