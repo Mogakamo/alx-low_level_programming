ac2c920af5a326acd1d89fd2c387ba38b5a4cb2e
Mogakamo <mogaka.amo254@gmail.com>
2021-07-12 18:10:13 +0300

    added a program that prints the argument of the program it is running

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,16 +3,16 @@
 /**
  * main - Entry point of the program
  * Return: 0
- * argc: arguments in the command line
- * argv: array of argc
+ * @argc: arguments in the command line
+ * @argv: array of argc
  */
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 	int i;
 
 	for (i = 0; i < argc; i++)
 	{
-		printf("%s\n", argv[i]);		
+		printf("%s\n", argv[i]);
 	}
 
 	return (0);
