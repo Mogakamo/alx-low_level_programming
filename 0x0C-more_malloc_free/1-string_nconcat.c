4d48e444ab77e54d525bcbf9ee12dc082f9172dc
Mogakamo <mogaka.amo254@gmail.com>
2021-07-15 22:25:30 +0300

    malloc 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,42 +1,49 @@
-#include "holberton.h"
 #include <stdlib.h>
-
 /**
- * string_nconcat - concatenates n number of s1 to s2
- *
- * @s1: first string
- * @s2: second string
- * @n: number of s2 to concatenate
- *
- * Return: s1_s2
- */
+  * string_nconcat - concatenates two strings.
+  * @s1: pointer to first string.
+  * @s2: pointer to second string from which we'll take first n bytes.
+  * @n: n bytes of s2.
+  *
+  * Return: pointer to newly allocated space in memory.
+  * NULL if malloc fails.
+  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-char *s1_s2;
-unsigned int size1, size2;
-unsigned int count, count2;
+	char *nstr;
+	unsigned int len1, len2, size, i, j;
 
-if (s1 == NULL)
-s1 = "";
-if (s2 == NULL)
-s2 = "";
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	len1 = 0;
+	while (s1[len1] != '\0')
+		len1++;
+	len2 = 0;
+	while (s2[len2] != '\0')
+		len2++;
 
-for (size1 = 0; s1[size1] != '\0'; size1++)
-;
-for (size2 = 0; s2[size2] != '\0'; size2++)
-;
-if (n < size2)
-size2 = n;
+	if (n >= len2)
+		n = len2;
+	size = len1 + n;
 
-s1_s2 = malloc(sizeof(char) * (size1 + size2 + 1));
-if (s1_s2 == NULL)
-return (NULL);
-
-for (count = 0; count < size1; count++)
-s1_s2[count] = s1[count];
-for (count2 = 0; count2 < size2; count2++, count++)
-s1_s2[count] = s2[count2];
-s1_s2[count] = '\0';
-
-return (s1_s2);
+	nstr = malloc((sizeof(char) * size) + 1);
+	if (nstr == NULL)
+		return (NULL);
+	i = 0;
+	while (i < len1)
+	{
+		nstr[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (i < size)
+	{
+		nstr[i] = s2[j];
+		i++;
+		j++;
+	}
+	nstr[i] = '\0';
+	return (nstr);
 }
