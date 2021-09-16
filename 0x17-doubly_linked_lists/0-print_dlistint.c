26a425eee77f91d2fa9bc0f36d02912953bc0334
Mogakamo <mogaka.amo254@gmail.com>
2021-09-16 08:36:08 -0700

    0 - print list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - fuction that prints all the elements of dlistint_t list
+ * @h: points to the head of the list
+ * Description: shows the number of elements
+ *
+ * Return: the number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	const dlistint_t *list = h;
+	size_t size = 0;
+
+	while (list)
+	{
+		printf("%i\n", list->n);
+		size++;
+	}	
+	return (size);
+}
