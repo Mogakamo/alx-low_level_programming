24559cc6ea6bbd026de1dce4d9172b27aada22da
Mogakamo <com02519@uoeld.ac.ke>
2022-01-18 19:31:51 -0800

    working on the jump search algorithm

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,13 @@
+#include "search_algos.h"
+
+/**
+ * jump_search - Searches for a value in a sorted array
+ * @array: pointer to the first element in the array
+ * @size: number of the elements in the array
+ * @value: value to be searched for
+ */
+int jump_search(int *array, size_t size, int value)
+{
+	size_t i = 0;
+
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -8,4 +8,6 @@
 int linear_search(int *array, size_t size, int value);
 /* 1 - Binary Search */
 int binary_search(int *array, size_t size, int value);
+/* 7 - Jump Search */
+int jump_search(int *array, size_t size, int value);
 #endif
